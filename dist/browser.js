7e7a02004e3d2eee7125a274f297d976595ad067
Tim <timurvalishev@gmail.com>
2024-05-02 04:06:51 +0000

    ?

--- dist/browser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var legoBoost_1 = require("./legoBoost");
-var boost = new legoBoost_1.default();
-// @ts-ignore
-window.boost = boost;
-// @ts-ignore
-boost.logDebug = console.log;
-//# sourceMappingURL=browser.js.map
